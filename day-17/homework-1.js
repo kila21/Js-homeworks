ba02bc219a8bf801aa06316c67d70fdcd6a69afd
luka <luka.kilasonia.1@btu.edu.ge>
2021-09-08 17:36:21 +0400

    add day-17 homework1

---
+++ day-17/homework-1.js
@@ -0,0 +1,22 @@
+function getDivisors(number) {
+  const array = [];
+  if (typeof number !== "number") {
+    throw new Error("parameter type is not a number");
+    
+  } else if (number === 0) {
+    throw new Error("parameter can't be a 0");
+  } else {
+    let divisors = number;
+    while (divisors > 0) {
+      let result = number / divisors;
+      if (result === parseInt(result)) {
+        array.push(result);
+      }
+      divisors--;
+    }
+    console.log(array);
+  }
+}
+getDivisors(12);
+getDivisors("content");
+getDivisors(0);
