9225f28967141da96c577cf46db6ce25e39acd7f
luka <luka.kilasonia.1@btu.edu.ge>
2021-09-11 05:41:05 +0400

    homework-3

---
+++ day-18/arrays/homework-3.js
@@ -0,0 +1,34 @@
+function rotate(arr, num, side) {
+  if (!Array.isArray(arr)) {
+    throw new Error("first parameter should be an array");
+  } else if (!(typeof num === "number")) {
+    throw new Error("second parameter should be a number");
+  } else if (side && !(typeof side === "string")) {
+    throw new Error("third parameter should be a string");
+  } else {
+    let i = 0;
+    if (side === "left") {
+      while (i < num) {
+        let number = arr.shift();
+        arr.push(number);
+        i++;
+      }
+    } else {
+      while (i < num) {
+        let number = arr.pop();
+        arr.unshift(number);
+        i++;
+      }
+    }
+    console.log(arr);
+  }
+}
+
+try {
+  const arr = [1, 2, 3];
+  rotate(arr, 1, "left"); // result: [2,3,1]
+  // rotate(arr, 1); // result: [3,1,2]
+  //   rotate(arr, 2); // result: [2,3,1]
+} catch (err) {
+  console.log(err);
+}
