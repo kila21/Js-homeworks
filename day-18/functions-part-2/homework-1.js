54496f62fc3fa81e8e0b665c27054e61ec86c845
luka <luka.kilasonia.1@btu.edu.ge>
2021-09-13 06:14:05 +0400

    day-18 functions homework-1

---
+++ day-18/functions-part-2/homework-1.js
@@ -0,0 +1,38 @@
+function mix(...arg) {
+  let result;
+  const object = { errors: [], value: arg.length - 1 };
+
+  for (let i = 0; i < arg.length; i++) {
+    try {
+      if (i === 0) result = arg[i]();
+      else {
+        result = arg[i](result);
+      }
+    } catch (err) {
+      let errorObject = {};
+      errorObject["name"] = err.name;
+      errorObject["message"] = err.message;
+      errorObject["stack"] = err.stack;
+      errorObject["level"] = i;
+      object.errors.push(errorObject);
+    }
+  }
+  return object;
+}
+
+const result = mix(
+  () => {
+    return 0;
+  },
+  (prev) => {
+    return prev + 1;
+  },
+  (prev) => {
+    throw new RangeError("Range is wrong");
+  },
+  (prev) => {
+    return prev * 3;
+  }
+);
+
+console.log(result);
