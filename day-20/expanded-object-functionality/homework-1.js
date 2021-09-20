3084b1354f9cc6bb17817dbcb1c3d2f5bedd8fc7
luka <luka.kilasonia.1@btu.edu.ge>
2021-09-20 14:26:05 +0400

    day-20 homework

---
+++ day-20/expanded-object-functionality/homework-1.js
@@ -0,0 +1,47 @@
+Object.prototype.mergeDeepRight = function (data) {
+  for (let prop in data) {
+    if (typeof data[prop] === "object") {
+      if (!(prop in this)) {
+        this[prop] = {};
+      }  else if (Array.isArray(data[prop])) {
+        for (i of data[prop]) {
+          this[prop].unshift(i)
+        }
+      }
+      this[prop].mergeDeepRight(data[prop]);
+    } else if (typeof data[prop] === "function") {
+      return;
+    } else {
+      this[prop] = data[prop];
+    }
+  }
+};
+
+const data = {
+  name: "fred",
+  age: 10,
+  contact: {
+    email: "moo@example.com",
+    meta: {
+      verified: true,
+      tags: ["important"],
+    },
+  },
+};
+
+Object.defineProperty(data, mergeDeepRight(),{
+  enumerable: false,
+})
+data.mergeDeepRight({
+  age: 40,
+  contact: {
+    email: "baa@example.com",
+    favorite: true,
+    meta: {
+      tags: ["vip"],
+    },
+  },
+});
+
+console.log(data);
+console.log(data.contact);
